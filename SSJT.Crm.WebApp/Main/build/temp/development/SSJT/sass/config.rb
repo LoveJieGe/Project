0c70a57df54d5a4b6a469b0f012e9e99eb123278
stjworkemail <stjworkemail@163.com>
2018-03-21 22:57:40 +0800

    便签

--- SSJT.Crm.WebApp/Main/build/temp/development/SSJT/sass/config.rb
+++ SSJT.Crm.WebApp/Main/build/temp/development/SSJT/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'F:\MyProject\Git\Source\Project\SSJT.Crm.WebApp\Main/build/.sass-cache'
+cache_path = 'F:\C_Project\Git\Source\Project\SSJT.Crm\SSJT.Crm.WebApp\Main/build/.sass-cache'

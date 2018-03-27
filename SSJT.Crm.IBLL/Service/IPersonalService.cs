e613c88a2c7d69eea5757acc5fef2eb8de9ae6bf
stjworkemail <stjworkemail@163.com>
2018-03-27 22:10:49 +0800

    便签

---
+++ SSJT.Crm.IBLL/Service/IPersonalService.cs
@@ -0,0 +1,15 @@
+﻿using SSJT.Crm.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSJT.Crm.IBLL.Service
+{
+    [AjaxClass("SSJT.Crm.BLL.PersonalService,SSJT.Crm.BLL")]
+    public interface IPersonalService
+    {
+        Model.PersonalNote InsertData(Model.PersonalNote model);
+    }
+}

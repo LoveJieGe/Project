b1e4a38cc9cd5b4b39ef5f4657d92eebaf5b291b
songtaojie <songtaojie@aio5.com>
2018-04-28 16:42:57 +0800

    修改地址测试

---
+++ SSJT.Crm.Test/BalancesTest.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSJT.Crm.Test
+{
+    public class BalancesTest
+    {
+        public static void  Convert()
+        {
+            JsonSerializer serizer = new JsonSerializer();
+            string jsonStr = "[{ \"Header\":\"\u57fa\u672c\u4fe1\u606f\",\"ColSpan\":2},{ \"Header\":\"\u671f\u521d\u4f59\u989d\",\"ColSpan\":2},{ \"Header\":\"\u672c\u671f\u53d1\u751f\",\"ColSpan\":2},{ \"Header\":\"\u671f\u672b\u4f59\u989d\",\"ColSpan\":2}]";
+            JsonReader reader = new JsonTextReader(new StringReader(jsonStr));
+            object value = serizer.Deserialize(reader, typeof(List<Query>));
+        }
+        
+    }
+    public class Query
+    {
+        public string Header { get; set; }
+        public string ColSpan { get; set; }
+    }
+}

ff0df08dd17a5fe9f09053ce074df4fb220ac21f
stjworkemail <stjworkemail@163.com>
2018-05-22 23:22:24 +0800

    编辑

--- SSJT.Crm.WebApp/Main/packages/Common/src/field/UEditor.js
+++ SSJT.Crm.WebApp/Main/packages/Common/src/field/UEditor.js
@@ -1,3 +1,28 @@
 Ext.define('Common.field.UEditor', {
-    extend:'Ext.data.'
-})
+    extend:'Ext.field.Text',
+    requires:[
+        'Common.util.ResourceManager'
+    ],
+    xtype:'com_ueditor',
+    config:{
+        resizable:{
+            edges:'e s'
+        },
+        ue:{
+            lazy:true,
+            $value:true
+        }
+    },
+    clearable:false,
+
+    afterRender(){
+        const me = this;
+        me.callParent(arguments);
+
+        const input = me.inputElement,
+            pNode = input.dom.parentNode,
+            bundleId = 'ueditorbundle';
+        pNode.style.setProperty('width','100%');
+
+    }
+});
